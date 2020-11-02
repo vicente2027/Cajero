b6c7059052f6ee070c9e5838426c3ffd2314608c
vicente2027 <vicente2027@gmail.com>
2020-11-01 20:38:21 -0600

    Juan

--- Cajero/src/cajero/Cajero.java
+++ Cajero/src/cajero/Cajero.java
@@ -16,6 +16,8 @@ public class Cajero {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Juan");
     }
     
 }
